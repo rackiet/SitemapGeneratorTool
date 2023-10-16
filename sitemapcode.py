3a8db43cab3fb6795e5686dd2077a083dd313540
rackiet <rianandriyanto8@gmail.com>
2023-10-16 17:46:43 +0700

    Update sitemapcode.py

--- sitemapcode.py
+++ sitemapcode.py
@@ -1,17 +1,18 @@
 import datetime
 import random
+import sys
+import tkinter as tk
+from tkinter import filedialog, simpledialog, messagebox
 from xml.etree.ElementTree import Element, SubElement, ElementTree, tostring
 from xml.dom.minidom import parseString
-import tkinter as tk
-from tkinter import filedialog, messagebox, simpledialog
+import tkinter.ttk as ttk
 
 def pretty_xml(element):
-    """Return a pretty-printed XML string for the Element."""
     rough_string = tostring(element, 'utf-8')
     reparsed = parseString(rough_string)
     return reparsed.toprettyxml(indent="\t")
 
-def generate_sitemap_from_links(links, start_date_str, limit_per_sitemap, sitemap_name):
+def generate_sitemap_from_links(links, start_date_str, limit_per_sitemap, sitemap_name, progress_var):
     start_date = datetime.datetime.strptime(start_date_str, "%d/%m/%Y").date()
     today = datetime.date.today()
 
@@ -22,15 +23,17 @@ def generate_sitemap_from_links(links, start_date_str, limit_per_sitemap, sitema
     links_counter = 0
     current_sitemap_links = []
     current_date = start_date
+
     links_for_current_day = links_per_day + remainder
 
-    for link in links:
+    total_links = len(links)
+    for idx, link in enumerate(links):
         hour = random.randint(0, 23)
         minute = random.randint(0, 59)
         second = random.randint(0, 59)
         current_datetime = datetime.datetime.combine(current_date, datetime.time(hour, minute, second))
         current_sitemap_links.append((link.strip(), current_datetime.isoformat()))
-        
+
         links_for_current_day -= 1
         if links_for_current_day == 0:
             current_date += datetime.timedelta(days=1)
@@ -42,6 +45,8 @@ def generate_sitemap_from_links(links, start_date_str, limit_per_sitemap, sitema
             links_counter = 0
             current_sitemap_links = []
             sitemap_counter += 1
+
+        progress_var.set((idx + 1) / total_links * 100)
 
     if current_sitemap_links:
         save_to_xml(current_sitemap_links, f"{sitemap_name}{sitemap_counter}.xml")
@@ -55,34 +60,43 @@ def save_to_xml(links, filename):
         lm = SubElement(url, "lastmod")
         lm.text = lastmod
 
-    with open(filename, "w") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         f.write(pretty_xml(urlset))
 
-def load_links():
-    filepath = filedialog.askopenfilename(title="Open link.txt", filetypes=[("Text files", "*.txt")])
-    with open(filepath, 'r') as file:
-        return file.readlines()
+def start_generation():
+    file_path = filedialog.askopenfilename(title="Pilih file link.txt", filetypes=[("Text files", "*.txt")])
+    if not file_path:
+        return
 
-def generate_sitemap():
-    links = load_links()
+    with open(file_path, "r", encoding="utf-8") as file:
+        all_links = file.readlines()
+
     start_date_input = simpledialog.askstring("Input", "Masukkan tanggal mulai (dd/mm/yyyy):")
+    if not start_date_input:
+        return
+
     limit = simpledialog.askinteger("Input", "Berapa link maksimal per sitemap?")
+    if not limit:
+        return
+
     sitemap_name = simpledialog.askstring("Input", "Nama sitemap (tanpa .xml):")
+    if not sitemap_name:
+        return
 
-    generate_sitemap_from_links(links, start_date_input, limit, sitemap_name)
-    messagebox.showinfo("Success", "Sitemap has been generated!")
+    generate_sitemap_from_links(all_links, start_date_input, limit, sitemap_name, progress_var)
+    messagebox.showinfo("Info", "Sitemap berhasil digenerate!")
 
-def main():
-    root = tk.Tk()
-    root.title("Sitemap Generator")
-    
-    frame = tk.Frame(root)
-    frame.pack(padx=10, pady=10)
+app = tk.Tk()
+app.title("Sitemap Generator")
 
-    load_btn = tk.Button(frame, text="Generate Sitemap", command=generate_sitemap)
-    load_btn.pack(pady=20)
+frame = tk.Frame(app)
+frame.pack(pady=20)
 
-    root.mainloop()
+btn = tk.Button(frame, text="Generate Sitemap", command=start_generation)
+btn.pack()
 
-if __name__ == "__main__":
-    main()
+progress_var = tk.DoubleVar()
+progress = ttk.Progressbar(frame, orient="horizontal", length=300, variable=progress_var, mode="determinate")
+progress.pack(pady=20)
+
+app.mainloop()
