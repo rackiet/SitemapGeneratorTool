514daebb85ce356e2514e7f7fcd0a1d60972e1f1
rackiet <rianandriyanto8@gmail.com>
2023-10-16 17:27:01 +0700

    Update sitemapcode.py

--- sitemapcode.py
+++ sitemapcode.py
@@ -1,87 +1,88 @@
-import xml.etree.ElementTree as ET
-from datetime import datetime, timedelta
-from tqdm import tqdm
+import datetime
+import random
+from xml.etree.ElementTree import Element, SubElement, ElementTree, tostring
+from xml.dom.minidom import parseString
+import tkinter as tk
+from tkinter import filedialog, messagebox, simpledialog
 
-def validate_date(date_str):
-    try:
-        return datetime.strptime(date_str, '%d-%m-%Y').date()
-    except ValueError:
-        return None
+def pretty_xml(element):
+    """Return a pretty-printed XML string for the Element."""
+    rough_string = tostring(element, 'utf-8')
+    reparsed = parseString(rough_string)
+    return reparsed.toprettyxml(indent="\t")
 
-def url_generator(links, start_date, end_date, priority, changefreq):
-    current_day = start_date
-    link_count = 0
-    
-    while current_day <= end_date:
-        for link in links:
-            for minute in range(0, 59):
-                url_data = {
-                    'loc': link.strip(),
-                    'lastmod': current_day.strftime('%Y-%m-%d') + f"T{minute:02d}:00:00Z",
-                    'changefreq': changefreq,
-                    'priority': str(priority)
-                }
-                
-                yield url_data
+def generate_sitemap_from_links(links, start_date_str, limit_per_sitemap, sitemap_name):
+    start_date = datetime.datetime.strptime(start_date_str, "%d/%m/%Y").date()
+    today = datetime.date.today()
 
-                link_count += 1
-                if link_count >= 500:
-                    return
+    days_difference = (today - start_date).days + 1
+    links_per_day, remainder = divmod(len(links), days_difference)
 
-        current_day += timedelta(days=1)
+    sitemap_counter = 1
+    links_counter = 0
+    current_sitemap_links = []
+    current_date = start_date
+    links_for_current_day = links_per_day + remainder
 
-def create_sitemap_chunk(url_gen, filename):
-    urlset = ET.Element('urlset', xmlns='http://www.sitemaps.org/schemas/sitemap/0.9')
+    for link in links:
+        hour = random.randint(0, 23)
+        minute = random.randint(0, 59)
+        second = random.randint(0, 59)
+        current_datetime = datetime.datetime.combine(current_date, datetime.time(hour, minute, second))
+        current_sitemap_links.append((link.strip(), current_datetime.isoformat()))
+        
+        links_for_current_day -= 1
+        if links_for_current_day == 0:
+            current_date += datetime.timedelta(days=1)
+            links_for_current_day = links_per_day
 
-    for url_data in url_gen:
-        url = ET.SubElement(urlset, 'url')
-        for key, value in url_data.items():
-            elem = ET.SubElement(url, key)
-            elem.text = value
-    
-    tree = ET.ElementTree(urlset)
-    tree.write(filename, xml_declaration=True, encoding='utf-8')
+        links_counter += 1
+        if links_counter == limit_per_sitemap:
+            save_to_xml(current_sitemap_links, f"{sitemap_name}{sitemap_counter}.xml")
+            links_counter = 0
+            current_sitemap_links = []
+            sitemap_counter += 1
+
+    if current_sitemap_links:
+        save_to_xml(current_sitemap_links, f"{sitemap_name}{sitemap_counter}.xml")
+
+def save_to_xml(links, filename):
+    urlset = Element("urlset", xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
+    for link, lastmod in links:
+        url = SubElement(urlset, "url")
+        loc = SubElement(url, "loc")
+        loc.text = link
+        lm = SubElement(url, "lastmod")
+        lm.text = lastmod
+
+    with open(filename, "w") as f:
+        f.write(pretty_xml(urlset))
+
+def load_links():
+    filepath = filedialog.askopenfilename(title="Open link.txt", filetypes=[("Text files", "*.txt")])
+    with open(filepath, 'r') as file:
+        return file.readlines()
+
+def generate_sitemap():
+    links = load_links()
+    start_date_input = simpledialog.askstring("Input", "Masukkan tanggal mulai (dd/mm/yyyy):")
+    limit = simpledialog.askinteger("Input", "Berapa link maksimal per sitemap?")
+    sitemap_name = simpledialog.askstring("Input", "Nama sitemap (tanpa .xml):")
+
+    generate_sitemap_from_links(links, start_date_input, limit, sitemap_name)
+    messagebox.showinfo("Success", "Sitemap has been generated!")
 
 def main():
-    # Input and validate start and end dates
-    while True:
-        start_date_str = input("Enter the start date (DD-MM-YYYY): ")
-        end_date_str = input("Enter the end date (DD-MM-YYYY): ")
-        
-        start_date = validate_date(start_date_str)
-        end_date = validate_date(end_date_str)
-        
-        if start_date and end_date:
-            break
-        else:
-            print("Invalid date format. Please try again.")
+    root = tk.Tk()
+    root.title("Sitemap Generator")
     
-    # Input priority and changefreq
-    priority = float(input("Enter priority (e.g., 1.0): "))
-    changefreq = input("Enter changefreq (e.g., always, hourly, daily): ")
+    frame = tk.Frame(root)
+    frame.pack(padx=10, pady=10)
 
-    # Read website list from file
-    with open('listwebsite.txt', 'r') as f:
-        links = f.readlines()
+    load_btn = tk.Button(frame, text="Generate Sitemap", command=generate_sitemap)
+    load_btn.pack(pady=20)
 
-    sitemap_index = 1
-    total_links = len(links)
+    root.mainloop()
 
-    print(f"Generating sitemaps for {total_links} links...")
-
-    with tqdm(total=total_links) as pbar:
-        while links:
-            chunk_links = links[:500]
-            links = links[500:]
-
-            url_gen = url_generator(chunk_links, start_date, end_date, priority, changefreq)
-            filename = f'sitemap{sitemap_index}.xml'
-
-            create_sitemap_chunk(url_gen, filename)
-            pbar.update(len(chunk_links))
-
-            print(f"Sitemap chunk saved to {filename}")
-            sitemap_index += 1
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
